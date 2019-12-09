8770c96a1577a08b22c8c4b7342dbed184047ab7
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2019-12-09 14:58:24 +0100

    Update videodetector.m

--- videodetector.m
+++ videodetector.m
@@ -1,21 +1,40 @@
 figure(1)
-for img = 1:1:750;
-    data = imread(strcat('frame',num2str(img),'.jpg'));
-    diff = imsubtract(data(:,:,1), rgb2gray(data));
+vid = VideoReader('test1.mp4');
+nbr = vid.NumberOfFrames;
+
+for img = 1:nbr
+    %Diviser la video en images
+    video = strcat('frame',num2str(img),'.jpg');
+    image = read(vid, img);
+    imshow(image);
+    imwrite(image,video);
+    
+    %Lecture de chaque frame
+    image1 = imread(video);
+    
+    %
+    diamond = strel('diamond', 3);
+    erode = imerode(image1,diamond);
+    
+    %Soustraction de la couleur rouge
+    diff = imsubtract(erode(:,:,1), rgb2gray(erode));
     diff = im2bw(diff,0.15);
     diff = bwareaopen(diff,300);
     
     nb = bwlabel(diff, 8);
     box = regionprops(nb, 'BoundingBox', 'Centroid');
     
+    %Affichage de l'image
     subplot(111); 
-    imshow(data);
+    imshow(image1);
     hold on
  
+    %Boucle permettant d'afficher le rectangle
     for object = 1:length(box)
         bb = box(object).BoundingBox;
         rectangle('Position',bb,'EdgeColor','red','LineWidth',1)
     end
     hold off
+    
     drawnow;
 end
