f308ce988eb14bf9aef5719bf91454fe2f30daa5
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2019-12-09 12:07:34 +0100

    Update videodetector.m

--- videodetector.m
+++ videodetector.m
@@ -1,4 +1,17 @@
-% Diviser la video en plusieurs images
+%{
+X1 = imread('photo.jpg');
+SE = strel('diamond', 3);
+X2 = imerode(X1,SE);
+im_gray=rgb2gray(X2);
+im_R=X2(:,:,1);
+im_r=imsubtract(im_R,im_gray);
+
+figure(1)
+subplot(121); imshow(X1); title('Image couleur RGB');
+subplot(122); imshow(im_r); title('Image résultante');
+%}
+
+%{
 a=VideoReader('test.mp4');
 for img = 1:1:100;
     filename=strcat('frame',num2str(img),'.jpg');
@@ -7,3 +20,9 @@ for img = 1:1:100;
     imwrite(b,filename);
 end
 movie(img)
+%}
+
+a=VideoReader('test.mp4');
+for img = 1:1:100;
+    
+end
