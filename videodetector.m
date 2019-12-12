50482ebcd5d4625c826b60a0095857665dae887b
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2019-12-12 21:23:25 +0100

    Update videodetector.m

--- videodetector.m
+++ videodetector.m
@@ -1,27 +1,40 @@
-figure(1)
+
+%Ouvrir la fenetre
+fig = figure;
+%Importer la video
 vid = VideoReader('test1.mp4');
 get(vid);
+%Stocker le nombre de frame
 nbr = vid.NumberOfFrames;
 
-for img = 1:1:nbr
+%Enregistrer la video finale
+vidfile = VideoWriter('final');
+open(vidfile);
+
+%Debut du traitement de chaque frame
+for img = 1:nbr
     %Diviser la video en images
     image1 = read(vid, img);
-    imshow(image1,[]);
     
+    %Afficher chaque image de base
+    imshow(image1);
+    
+    %Meilleur contour pour les couleurs
     diamond = strel('diamond', 3);
     erode = imerode(image1,diamond);
     
     %Soustraction de la couleur rouge
     diff = imsubtract(erode(:,:,1), rgb2gray(erode));
+    %imshow(diff);
+    
+    %Garder seulement les zones voulues
     diff = im2bw(diff,0.15);
     diff = bwareaopen(diff,500);
+    %imshow(diff);
     
+    %Etiqueté chaque forme distinctement
     nb = bwlabel(diff, 8);
     box = regionprops(nb, 'BoundingBox', 'Centroid');
-    
-    %Affichage de l'image
-    subplot(111); 
-    imshow(image1);
     
     hold on
     %Boucle permettant d'afficher le rectangle
@@ -30,5 +43,14 @@ for img = 1:1:nbr
         rectangle('Position',bb,'EdgeColor','red','LineWidth',1)
     end
     hold off
+    
+    %Enregistrement de la nouvelle video
+    frm = getframe(fig);
+    writeVideo(vidfile,frm);
+    
+    %Afficher dans matlab
     drawnow;
 end
+
+%Fermer le fichier video ecrit
+close(vidfile);
